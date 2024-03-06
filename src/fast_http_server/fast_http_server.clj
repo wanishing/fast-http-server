872b9b7f592bcf6f6e9252ca053f77834db91bbb
talwanish <tal.wanish@riskified.com>
2024-03-06 10:44:02 +0200

    initial

---
+++ src/fast_http_server/fast_http_server.clj
@@ -0,0 +1,12 @@
+(ns fast-http-server.fast-http-server
+  "FIXME: my new org.corfield.new/scratch project.")
+
+(defn exec
+  "Invoke me with clojure -X fast-http-server.fast-http-server/exec"
+  [opts]
+  (println "exec with" opts))
+
+(defn -main
+  "Invoke me with clojure -M -m fast-http-server.fast-http-server"
+  [& args]
+  (println "-main with" args))
